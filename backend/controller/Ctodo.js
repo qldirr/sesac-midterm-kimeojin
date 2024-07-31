1deb0687ca1b46406fa54248e5fb2e3dafe5eccf
qldirr <sub05073@naver.com>
2024-07-31 17:54:14 +0900

    back : last commit

--- backend/controller/Ctodo.js
+++ backend/controller/Ctodo.js
@@ -10,7 +10,7 @@ exports.postTodo = async (req, res) => {
         res.json(newTodo)
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error')
+        res.status(500).json({message:'Internal Server Error'})
     }
 }
 
@@ -34,7 +34,8 @@ exports.getTodo = async (req, res) => {
                 id
             }
         })
-        res.json(getTodo)
+        if (getTodo) res.json(getTodo)
+            else res.json({message:'Todo not found'})
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error')
@@ -44,7 +45,23 @@ exports.getTodo = async (req, res) => {
 // 특정 ID의 Todo 수정
 exports.patchTodo = async (req, res) => {
     try {
-        
+        const {id} = req.params
+        const {title, done} = req.body
+        const patchTodo = await Todo.update(
+            {title, done},
+            {
+
+                where : {id},
+            }
+        )
+        if (patchTodo[0] === 1) {
+            const patchedTodo = await Todo.findOne({
+                where:{id}
+            })
+
+            res.json(patchedTodo)
+        }
+            else res.json({message:'Todo not found'})
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error')
@@ -54,7 +71,12 @@ exports.patchTodo = async (req, res) => {
 // 특정 ID의 Todo 삭제
 exports.deleteTodo = async (req, res) => {
     try {
-        
+        const {id} = req.params
+        const deleteTodo = await Todo.destroy({
+            where : {id}
+        })
+        if (deleteTodo === 1) res.json({deletedId:id, message:'Todo deleted successfully'})
+            else res.json({message:'Todo not found'})
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error')
