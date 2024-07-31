bb8015e0b551051685cce396b380b6039435fdd4
qldirr <sub05073@naver.com>
2024-07-31 15:40:40 +0900

    front : css 추가

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,38 +1,56 @@
 const todoList = document.querySelector('.todoList')
+const form = document.getElementById('todo-form')
+const todoInput = document.querySelector('input[name = "todoInput"]')
+const newTodo = todoInput.value.trim()
 
 window.addEventListener("DOMContentLoaded", (event) => {
     getTodos()
-  });
+});
 
 // 초기 Todo 목록 불러오기
-function getTodos(){
+function getTodos() {
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then(res => res.json())
         .then(json => {
-            console.log(json);
             const getTodoList = json.slice(0, 10)
             console.log('get', getTodoList);
             const li = document.createElement('li')
+            const span = document.createElement('span')
+            const delBtn = document.createElement('button')
+            delBtn.innerText = 'X'
 
-            todoList.innerHTML = `${getTodoList.title} - ${getTodoList.completed}`
+            getTodoList.forEach((value) => {
+                console.log('va', value);
+                span.innerHTML = value.title
+                li.appendChild(span)
+                li.appendChild(delBtn)
+                todoList.appendChild(li)
+            });
+
         })
-    
+
 }
 
 
-const form = document.getElementById('todo-form')
-const todos = document.querySelector('.todoList')
+
+function keyCodeCheck() {
+    if (window.event.keyCode === 13 && newTodo !== '') {
+        addTodo()
+    }
+}
 
 // Todo 추가
-function addTodo(){
-    const todoInput = document.querySelector('input[name = "todoInput"]')
-    const newTodo = todoInput.value.trim()
+function addTodo() {
 
-    if(newTodo !== ''){
-        const newTodoLi = document.createElement('li')
-        newTodoLi.append(newTodo)
-        todos.append(newTodoLi)
-    }
+
+    const newTodoLi = document.createElement('li')
+    const newBtn = document.createElement('button')
+    const newSpan = document.createElement('span')
+    newTodoLi.appendChild(newBtn)
+    newTodoLi.appendChild(newSpan)
+
+    newSpan.textContent = newTodo
+    todoList.appendChild(newTodoLi)
 
     todoInput.value = ''
 }
